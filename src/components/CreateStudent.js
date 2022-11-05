4c62fca7607599638e6584cda11d785510195166
Mike Wittenauer <mikewittenauer@me.com>
2022-11-05 16:38:20 -0400

    Create student add form

--- src/components/CreateStudent.js
+++ src/components/CreateStudent.js
@@ -1,9 +1,27 @@
 import React from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const CreateStudent = () => {
     return (
-        <div>
-            <p>Create Student Component!</p>
+        <div className="form-wrapper">
+            <Form>
+                <Form.Group controlId="Name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text"/>
+                </Form.Group>
+                <Form.Group controlId="Email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email"/>
+                </Form.Group>
+                <Form.Group controlId="Name">
+                <Form.Label>Roll No</Form.Label>
+                <Form.Control type="text"/>
+                </Form.Group>
+                <Button variant="danger" size="lg" block="block" type="submit">
+                Create Student
+                </Button>
+            </Form>
         </div>
     )
 }
