73bd651bbf84e82ce397f6d589eb6a7baef2e678
Mike Wittenauer <mikewittenauer@me.com>
2022-11-06 09:55:31 -0500

    Create routes for api

---
+++ backend/routes/students.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const Student = require('../models/Student');
+
+router.route('/create-student').post((req, res, next) => {
+    Student.create(req.body, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            console.log(data);
+            res.json(data);
+        }
+    })
+});
+
+router.route('/').get((req, res) => {
+    Student.find((error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.json(data);
+        }
+    })
+});
+
+router.route('/update-student/:id').put((req, res, next) => {
+    Student.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, (error, data) => {
+        if (error) {
+            console.log(error);
+            return next(error);
+        } else {
+            console.log('Student updated successfully !');
+            res.json(data);
+        }
+    })
+});
+
+router.route('/delete-student/:id').delete((req, res, next) => {
+    Student.findByIdAndRemove(req.params.id, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.json({ msg: data });
+        }
+    });
+});
