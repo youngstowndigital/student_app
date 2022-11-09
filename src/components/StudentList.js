adddb506bbde9692d27507a125b7ee038f8c7abe
Mike Wittenauer <mikewittenauer@me.com>
2022-11-09 07:47:19 -0500

    Implement student list when student is added

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+import StudentTableRow from './StudentTableRow';
 
 const StudentList = () => {
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/students/')
+            .then(res => {
+                setStudents(res.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
     return (
-        <div>
-            <p>Student List Component!</p>
+        <div className="table-wrapper">
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Roll No</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {students.map((res, i) => <StudentTableRow obj={res} key={i} />)}
+                </tbody>
+            </Table>
         </div>
-    )
+    );
 }
 
 export default StudentList;
