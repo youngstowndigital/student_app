eb4dccb69848afa86c180158c7be98ecc33f3688
Mike Wittenauer <mikewittenauer@me.com>
2022-11-10 07:48:37 -0500

    Delete student functionality

--- src/components/StudentTableRow.js
+++ src/components/StudentTableRow.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 const StudentTableRow = (props) => {
+    const deleteStudent = () => {
+        axios.delete(`http://localhost:4000/students/delete-student/${props.obj._id}`)
+            .then(res => {
+                console.log('Student successfully deleted');
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     return (
         <tr>
             <td>{props.obj.name}</td>
@@ -12,7 +23,7 @@ const StudentTableRow = (props) => {
                 <Link className="edit-link" to={"/edit-student/" + props.obj._id}>
                     Edit
                 </Link>
-                <Button size="sm" variant="danger">Delete</Button>
+                <Button size="sm" variant="danger" onClick={deleteStudent}>Delete</Button>
             </td>
         </tr>
     )
